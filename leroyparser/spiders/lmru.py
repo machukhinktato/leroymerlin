908e02ac82f93736a65b6dfc959155bd2bdc3fa9
machukhinktato <tarabrinmv@gmail.com>
2020-11-22 15:01:53 +0300

    in progress

--- leroyparser/spiders/lmru.py
+++ leroyparser/spiders/lmru.py
@@ -1,10 +1,11 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class LmruSpider(scrapy.Spider):
     name = 'lmru'
     allowed_domains = ['leroymerlin.ru']
-    start_urls = ['http://leroymerlin.ru/']
+    start_urls = ['https://leroymerlin.ru/search/?q=%D0%BE%D0%B1%D0%BE%D0%B8&family=00b9b5a0-faeb-11e9-810b-878d0b27ea5b&suggest=true']
 
-    def parse(self, response):
+    def parse(self, response:HtmlResponse):
         pass
