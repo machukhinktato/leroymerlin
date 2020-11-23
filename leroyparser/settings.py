7b220744a841a02e4198ca63fd929548091b61a2
machukhinktato <tarabrinmv@gmail.com>
2020-11-23 09:06:36 +0300

    in progress

--- leroyparser/settings.py
+++ leroyparser/settings.py
@@ -17,10 +17,10 @@ NEWSPIDER_MODULE = 'leroyparser.spiders'
 #USER_AGENT = 'leroyparser (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'leroyparser.pipelines.LeroyparserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'leroyparser.pipelines.LeroyparserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- leroyparser/spiders/lmru.py
+++ leroyparser/spiders/lmru.py
@@ -8,4 +8,5 @@ class LmruSpider(scrapy.Spider):
     start_urls = ['https://leroymerlin.ru/search/?q=%D0%BE%D0%B1%D0%BE%D0%B8&family=00b9b5a0-faeb-11e9-810b-878d0b27ea5b&suggest=true']
 
     def parse(self, response:HtmlResponse):
+        print()
         pass
