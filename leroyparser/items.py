b73607871998b9ff379d5fe2af188207c1eaa309
machukhinktato <tarabrinmv@gmail.com>
2020-11-27 17:06:33 +0300

    in progress

--- leroyparser/items.py
+++ leroyparser/items.py
@@ -11,6 +11,8 @@ class LeroyparserItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field(output_processor=TakeFirst())
     price = scrapy.Field(output_processor=TakeFirst())
+    description = scrapy.Field(output_processor=TakeFirst())
+    _id = scrapy.Field()
     photo = scrapy.Field(input_proccesor=MapCompose())
     url = scrapy.Field(output_processor=TakeFirst())
     print()
